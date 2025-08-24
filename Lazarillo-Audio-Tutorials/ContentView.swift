dce73bc526c701a5aec4b04849d342215453ac1c
joanna <joanna.luberadzka@eurecat.org>
2025-08-24 16:25:13 +0200

    Initial Commit

---
+++ Lazarillo-Audio-Tutorials/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Lazarillo-Audio-Tutorials
+//
+//  Created by Joanna Luberadzka on 24/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lazarillo-Audio-Tutorials/Lazarillo_Audio_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lazarillo_Audio_TutorialsApp.swift
+//  Lazarillo-Audio-Tutorials
+//
+//  Created by Joanna Luberadzka on 24/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Lazarillo_Audio_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
