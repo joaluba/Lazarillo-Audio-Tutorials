7741e389f370b94ac3c63e945c83ca8a93d61902
joanna <joanna.luberadzka@eurecat.org>
2025-09-05 13:56:50 +0200

    basic audio functionalities divided into: normal playback, spatializer, panning, reverb, and speech synth

--- Lazarillo-Audio-Tutorials/AudioManager.swift
+++ Lazarillo-Audio-Tutorials/AudioManager.swift
@@ -8,65 +8,61 @@
 import Foundation
 import AVFoundation
 
-class AudioManager{
+
+class AudioManager: ObservableObject{
     
     // --- initialize speech synthesizer class ---
-    private var MySynthesizer = AVSpeechSynthesizer()
+    public var MySynthesizer = AVSpeechSynthesizer()
     
     // --- initialize empty basic audio player ---
     // (using ? is the way to initialize it without specific url - this is the standard way to
     // declare a basic audio player in a class when we don’t know the audio file at initialization time)
-    private var MyAudioPlayer = AVAudioPlayer()
+    public var MyAudioPlayer = AVAudioPlayer()
     
     // --- initialize audio engine and its nodes ---
-    private var MyEngine = AVAudioEngine()
-    private var MyAudioPlayerNode = AVAudioPlayerNode()
-    private var MyEnvironmentNode = AVAudioEnvironmentNode()
-    private var MyReverb = AVAudioUnitReverb()
+    public var MyEngine = AVAudioEngine()
+    public var MyAudioPlayerNode = AVAudioPlayerNode()
+    public var MyEnvironmentNode = AVAudioEnvironmentNode()
+    public var MyReverb = AVAudioUnitReverb()
+
     
-    // ------ set up signal flow in the audio engine ------
-    func setupAudioEngine() {
+    // ------ Function to play back audio from a file using Audio Engine ------
+    func play_audio_file_in_space(filename: String, direction: Float) {
         
+        // if the something is already playing back, stop it
+        if MyAudioPlayerNode.isPlaying || MyAudioPlayer.isPlaying || MySynthesizer.isSpeaking{
+            MySynthesizer.stopSpeaking(at: .immediate)
+            MyAudioPlayerNode.stop()
+            MyAudioPlayer.stop()
+        }
+        // set up audio engine
         MyEngine.attach(MyAudioPlayerNode)
         MyEngine.attach(MyEnvironmentNode)
-        MyEngine.attach(MyReverb)
 
         // Player -> Spatializer -> Output
         MyEngine.connect(MyAudioPlayerNode, to: MyEnvironmentNode, format: nil)
-        MyEngine.connect(MyEnvironmentNode, to: MyReverb, format: nil)
-        MyEngine.connect(MyReverb, to: MyEngine.mainMixerNode, format: nil)
+        MyEngine.connect(MyEnvironmentNode, to: MyEngine.mainMixerNode, format: nil)
     
-        MyEngine.mainMixerNode.outputVolume = 0.1
+        MyEngine.mainMixerNode.outputVolume = 1
+
+        MyAudioPlayerNode.pan = 0
         
         do {
             try MyEngine.start()
         } catch {
             print("Error starting engine: \(error)")
         }
-    }
-    
-    // ------ Function to play back audio from a file using Audio Engine ------
-    func play_audio_file_in_space(filename: String, direction: Float) {
         
-        if MyAudioPlayerNode.isPlaying {
-            MyAudioPlayerNode.stop()
-        }
-        
-        // set up audio engine
-        setupAudioEngine()
-        
-        // Place listener at the origin
+        // Place listener at the origin (looking to the front)
         MyEnvironmentNode.listenerPosition = AVAudio3DPoint(x: 0, y: 0, z: 0)
+        MyEnvironmentNode.listenerAngularOrientation = AVAudio3DAngularOrientation(
+           yaw: 0, pitch: 0, roll: 0)
         
-        // Place sound source in a circle around listener
+        // Place source in the environment
         let radians = direction * .pi / 180
         let x = cos(radians) * 2.0
         let z = sin(radians) * 2.0
         MyAudioPlayerNode.position = AVAudio3DPoint(x: x, y: 0, z: z)
-        
-        // Configure reverb
-        MyReverb.loadFactoryPreset(.mediumRoom)
-        MyReverb.wetDryMix = 50 // percentage of effect applied
         
         // guard let ... else { ... return } =
         // if either the URL or audio file could not be created, stop and exit the function
@@ -86,13 +82,125 @@ class AudioManager{
         }
     }
     
+    
+    func play_audio_file_with_reverb(filename: String, wetdry: Float) {
+        
+        // if the something is already playing back, stop it
+        if MyAudioPlayerNode.isPlaying || MyAudioPlayer.isPlaying || MySynthesizer.isSpeaking{
+            MySynthesizer.stopSpeaking(at: .immediate)
+            MyAudioPlayerNode.stop()
+            MyAudioPlayer.stop()
+        }
+        
+        // set up audio engine
+        MyEngine.attach(MyAudioPlayerNode)
+        MyEngine.attach(MyReverb)
+
+        // Player -> Reverb -> Output
+        MyEngine.connect(MyAudioPlayerNode, to: MyReverb, format: nil)
+        MyEngine.connect(MyReverb, to: MyEngine.mainMixerNode, format: nil)
+    
+        MyEngine.mainMixerNode.outputVolume = 1
+        
+        MyAudioPlayerNode.pan = 0
+        
+        do {
+            try MyEngine.start()
+        } catch {
+            print("Error starting engine: \(error)")
+        }
+        
+        // Configure reverb node
+        MyReverb.loadFactoryPreset(.mediumRoom)
+        MyReverb.wetDryMix = wetdry // percentage of effect applied
+        
+        // guard let ... else { ... return } =
+        // if either the URL or audio file could not be created, stop and exit the function
+        guard let url = Bundle.main.url(forResource: filename, withExtension: ".wav"),
+              let audioFile = try? AVAudioFile(forReading: url) else {
+            print("File not found: \(filename)")
+            return
+        }
+        
+        do {
+            try MyEngine.start()
+            MyAudioPlayerNode.scheduleFile(audioFile, at: nil, completionHandler: nil)
+            MyAudioPlayerNode.play()
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+    
+    
+    func play_audio_file_panned(filename: String, direction: Float) {
+        
+        // if the something is already playing back, stop it
+        if MyAudioPlayerNode.isPlaying || MyAudioPlayer.isPlaying || MySynthesizer.isSpeaking{
+            MySynthesizer.stopSpeaking(at: .immediate)
+            MyAudioPlayerNode.stop()
+            MyAudioPlayer.stop()
+        }
+        
+        // set up audio engine
+        MyEngine.attach(MyAudioPlayerNode)
+
+        // Player (with panning) -> Output
+        MyEngine.connect(MyAudioPlayerNode, to: MyEngine.mainMixerNode, format: nil)
+    
+        MyEngine.mainMixerNode.outputVolume = 1
+        
+        do {
+            try MyEngine.start()
+        } catch {
+            print("Error starting engine: \(error)")
+        }
+        
+        // Panning with 2 cardioids
+        let radians_R=direction-40.0*(.pi/180);
+        let radians_L=direction+40.0*(.pi/180);
+        // compute gain for left and right
+        let G_R=abs(1/2*(1+cos(radians_R)));
+        let G_L=abs(1/2*(1+cos(radians_L)));
+        print("gain")
+        print(G_R)
+        print(G_L)
+        // normalized gain [0,1]
+        let G_R_norm=G_R/(G_R+G_L)
+        let G_L_norm=G_L/(G_R+G_L)
+        print("gain norm")
+        print(G_R_norm)
+        print(G_L_norm)
+        // panning parameter [-1,1]
+        let panLR=G_R_norm-G_L_norm
+        
+        // Left/Right Panning
+        MyAudioPlayerNode.pan = panLR
+
+        // guard let ... else { ... return } =
+        // if either the URL or audio file could not be created, stop and exit the function
+        guard let url = Bundle.main.url(forResource: filename, withExtension: ".wav"),
+              let audioFile = try? AVAudioFile(forReading: url) else {
+            print("File not found: \(filename)")
+            return
+        }
+        
+        do {
+            try MyEngine.start()
+            MyAudioPlayerNode.scheduleFile(audioFile, at: nil, completionHandler: nil)
+            MyAudioPlayerNode.play()
+        } catch {
+            print("Error: \(error)")
+        }
+    }
 
     // ------ Function to synthesize and play back speech ------
     func synth_and_play_speech(text: String) {
         
-        // if the synth is already playing back something, stop it
-        if MySynthesizer.isSpeaking{
+        // if the something is already playing back, stop it
+        if MyAudioPlayerNode.isPlaying || MyAudioPlayer.isPlaying || MySynthesizer.isSpeaking{
             MySynthesizer.stopSpeaking(at: .immediate)
+            MyAudioPlayerNode.stop()
+            MyAudioPlayer.stop()
         }
 
         // Create the utterance
@@ -119,7 +227,6 @@ class AudioManager{
             do {
                 // Fill MyAudioPlayer with a specific audio url
                 MyAudioPlayer = try AVAudioPlayer(contentsOf: url)
-                
                 // Playback
                 MyAudioPlayer.prepareToPlay() //optional, to reduce latency
                 MyAudioPlayer.play()
--- Lazarillo-Audio-Tutorials/BasicAudioView.swift
+++ Lazarillo-Audio-Tutorials/BasicAudioView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct BasicAudio: View {
     
     // initialize AudioManager class (contains my functions for playback)
-    let audioManager = AudioManager()
+    @StateObject private var audioManager = AudioManager()
     
     // initialize string that will be printed while the synthesizer is speaking
     @State private var text_to_speak: String = ""
@@ -18,18 +18,39 @@ struct BasicAudio: View {
     // initialize var for audio file name to play
     @State private var myaudiofilename: String = ""
     
-    // initialize var for audio file name to play
+    // initialize var for direction of arrival of the source
     @State private var mydirection: Float = 0.0
+    
+    // initialize var for reverb wetdry parameter
+    @State private var mywetdry: Float = 0.0
+    
+    // initialize var for reverb wetdry parameter
+    @State private var sectionplaying: String = ""
     
     
     var body: some View {
+        
         VStack {
             Text("Basic audio functions")
+            playFileSection
+            playReverbSection
+            playSpatialSection
+            playPannedSection
+            ttsSection
+        }
+    }
+    
+    
+    // ---------- playFileSection ----------
+    var playFileSection: some View {
+        VStack {
             
             Button(action: {
                 
                 myaudiofilename="speech_sample"
                 audioManager.play_audio_file(filename: myaudiofilename)
+                sectionplaying="playFileSection"
+                
             }) {
                 Text("Play audio from file")
                     .font(.headline)
@@ -39,19 +60,51 @@ struct BasicAudio: View {
                     .cornerRadius(10)
             }
             
-            if !myaudiofilename.isEmpty {
-                Text("Playing back: \(myaudiofilename).wav")
+            if sectionplaying=="playFileSection" {
+                Text("Playing back: \(myaudiofilename).wav ")
                     .padding()
                     .multilineTextAlignment(.center)
             }
+        }
+    }
+    
+    // ---------- playReverbSection ----------
+    var playReverbSection: some View {
+        VStack {
             
+            Button(action: {
+                mywetdry=Float.random(in: 0...100)
+                myaudiofilename="speech_sample"
+                audioManager.play_audio_file_with_reverb(filename: myaudiofilename, wetdry: mywetdry)
+                sectionplaying="playReverbSection"
+            }) {
+                Text("Play audio from file with reverb")
+                    .font(.headline)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            
+            if sectionplaying=="playReverbSection"  {
+                Text("Playing back: \(myaudiofilename).wav with wetdry mix \(Int(mywetdry)) %")
+                    .padding()
+                    .multilineTextAlignment(.center)
+            }
+        }
+    }
+    
+    // ---------- playSpatialSection ----------
+    var playSpatialSection: some View {
+        VStack {
             
             Button(action: {
                 mydirection=Float.random(in: 0...360)
                 myaudiofilename="speech_sample"
-                audioManager.play_audio_file_in_space(filename: myaudiofilename, direction: mydirection )
+                audioManager.play_audio_file_in_space(filename: myaudiofilename, direction: mydirection)
+                sectionplaying="playSpatialSection"
             }) {
-                Text("Play audio from file")
+                Text("Play spatialized audio file")
                     .font(.headline)
                     .padding()
                     .foregroundColor(.white)
@@ -59,12 +112,44 @@ struct BasicAudio: View {
                     .cornerRadius(10)
             }
             
-            if !myaudiofilename.isEmpty {
-                Text("Playing back: \(myaudiofilename).wav")
+            if sectionplaying=="playSpatialSection" {
+                Text("Playing back: \(myaudiofilename).wav with DOA of \(Int(mydirection)) deg")
                     .padding()
                     .multilineTextAlignment(.center)
             }
             
+        }
+    }
+    
+    // ---------- playPannedSection ----------
+    var playPannedSection: some View {
+        VStack {
+            
+            Button(action: {
+                mydirection=Float.random(in: 0...360)
+                myaudiofilename="speech_sample"
+                audioManager.play_audio_file_panned(filename: myaudiofilename, direction: mydirection)
+                sectionplaying="playPannedSection"
+            }) {
+                Text("Play panned audio file")
+                    .font(.headline)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            
+            if  sectionplaying=="playPannedSection"  {
+                Text("Playing back: \(myaudiofilename).wav with DOA of \(Int(mydirection)) deg")
+                    .padding()
+                    .multilineTextAlignment(.center)
+            }
+        }
+    }
+    
+    // ---------- ttsSection ----------
+    var ttsSection: some View {
+        VStack {
             
             Button(action: {
                 // pick random direction & distance
@@ -75,6 +160,8 @@ struct BasicAudio: View {
                 text_to_speak="Waypoint at \(hour) o’clock, \(distance) meters away"
                 
                 audioManager.synth_and_play_speech(text: text_to_speak)
+                sectionplaying="ttsSection"
+
             }) {
                 Text("TTS - synth and play")
                     .font(.headline)
@@ -84,14 +171,16 @@ struct BasicAudio: View {
                     .cornerRadius(10)
             }
             
-            if !text_to_speak.isEmpty {
+            if sectionplaying=="ttsSection"{
                 Text("Spoken: \(text_to_speak)")
                     .padding()
                     .multilineTextAlignment(.center)
             }
             
-            
-            
         }
     }
+    
 }
+    
+    
+    
